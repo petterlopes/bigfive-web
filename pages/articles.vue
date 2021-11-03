5e216492c1d232b58027335b5d8fde2e5cba4896
petterlopes <petter.plopes@gmail.com>
2021-11-03 19:14:37 -0300

    atualizações

--- pages/articles.vue
+++ pages/articles.vue
@@ -2,27 +2,29 @@
   <div>
     <h1>Artigos</h1>
     PERITUM - Consultoria e Treinamento LTDA
-  <div>
-  <div>
-    <Wrapper>
-      <v-row
-        cols="12"
-        class="py-10"
-      >       
-        <v-col
-          cols="12"
-          xs="12"
-          md="6"
-          class="text-center"
-        >
-          <img
-            class="responsive"
-            alt="PERITUM - Consultoria e Treinamento LTDA"
-            :src="require('@/assets/PERITUM.png')"
+    <div>
+      <div>
+        <Wrapper>
+          <v-row
+            cols="12"
+            class="py-10"
           >
-        </v-col>
-      </v-row>
-    </Wrapper>    
+            <v-col
+              cols="12"
+              xs="12"
+              md="6"
+              class="text-center"
+            >
+              <img
+                class="responsive"
+                alt="PERITUM - Consultoria e Treinamento LTDA"
+                :src="require('@/assets/PERITUM.png')"
+              >
+            </v-col>
+          </v-row>
+        </Wrapper>
+      </div>
+    </div>
   </div>
 </template>
 <script>
