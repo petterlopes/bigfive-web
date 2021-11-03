d912e7f77691896be607ad676b8df0d8f9214741
petterlopes <petter.plopes@gmail.com>
2021-11-03 20:40:56 -0300

    correção na tradução

--- pages/agreeableness.vue
+++ pages/agreeableness.vue
@@ -12,35 +12,26 @@
     </v-img>
     <v-card-text>
       <p>
-        A responsabilidade reflete diferenças individuais em preocupação com a cooperação e a harmonia social.Indivíduos agradáveis valorizam-se com os outros.
-      </p>
+        A amabilidade reflete as diferenças individuais na preocupação com a cooperação e a harmonia social. Indivíduos agradáveis ​​valorizam se dar bem com os outros.      </p>
       <p>
-        Eles são, portanto, atenciosos, amigáveis, generosos, úteis e dispostos a comprometer seus interesses com os outros.
-        As pessoas agradáveis também têm uma visão otimista da natureza humana.
-        Eles acreditam que as pessoas são basicamente honestas, decentes e confiáveis.
-        Indivíduos desagradáveis colocam o interesse próprio acima com os outros.
-        Eles geralmente são despreocupados com o bem-estar dos outros e, portanto, são improváveis de se estenderem para outras pessoas.
-        Às vezes, seu ceticismo sobre os motivos dos outros faz com que eles sejam suspeitos, hostis e não cooperativos.
+        Eles são, portanto, atenciosos, amigáveis, generosos, prestativos e dispostos a comprometer seus interesses com os dos outros. Pessoas agradáveis ​​também têm uma visão otimista da natureza humana. Eles acreditam que as pessoas são basicamente honestas, decentes e confiáveis. Indivíduos desagradáveis ​​colocam o interesse próprio acima de se dar bem com os outros. Eles geralmente não se preocupam com o bem-estar dos outros e, portanto, é improvável que se estendam para outras pessoas. Às vezes, seu ceticismo sobre os motivos dos outros faz com que sejam desconfiados, hostis e não cooperativos.
       </p>
 
       <blockquote class="blockquote">
         <v-icon>{{ mdiFormatQuoteOpen }}</v-icon>
-        A responsabilidade reflete as diferenças individuais em preocupação com a cooperação e a harmonia social.
+        A amabilidade reflete as diferenças individuais na preocupação com a cooperação e harmonia social.
         <v-icon>{{ mdiFormatQuoteClose }}</v-icon>
       </blockquote>
 
       <p>
-        A agricultura é obviamente vantajosa para atingir e manter a popularidade.
-        As pessoas agradáveis são melhores do que pessoas desagradáveis.
-        Por outro lado, a agradaidade não é útil em situações que exigem decisões objetivas difíceis ou absolutas.
-        Pessoas desagradáveis podem fazer excelentes cientistas, críticos ou soldados.
+        A amabilidade é obviamente vantajosa para alcançar e manter a popularidade. Pessoas agradáveis ​​são mais queridas do que pessoas desagradáveis. Por outro lado, a afabilidade não é útil em situações que exigem decisões difíceis ou objetivas absolutas. Pessoas desagradáveis ​​podem ser excelentes cientistas, críticos ou soldados.
       </p>
 
       <div class="title">
         Confiar
       </div>
       <p>
-        Uma pessoa com alta confiança assume que a maioria das pessoas é justa, honesta e tem boas intenções.Pessoas baixas em confiança vêem os outros como egoístas, desonestos e potencialmente perigosos.
+        Pessoas com pontuação alta nessa escala não veem necessidade de fingimento ou manipulação ao lidar com outras pessoas e, portanto, são francas, francas e sinceras. Pessoas com pontuação baixa acreditam que uma certa dose de engano nas relações sociais é necessária. As pessoas acham relativamente fácil se relacionar com os diretores de pontuação mais alta nessa escala. Eles geralmente acham mais difícil se relacionar com os pontuadores menos diretos nesta escala. Deve ficar claro que indivíduos com pontuação baixa não são sem princípios ou imorais; eles são simplesmente mais cautelosos e menos dispostos a revelar abertamente toda a verdade.
       </p>
 
       <div class="title">
@@ -54,28 +45,28 @@
         Altruísmo
       </div>
       <p>
-        As pessoas altruístas acham ajudando outras pessoas genuinamente gratificantes.Consequentemente, eles geralmente estão dispostos a ajudar aqueles que estão precisando.As pessoas altruístas acham que fazer coisas para os outros é uma forma de auto-realização em vez de auto-sacrifício.Baixos marcadores nesta escala não são particularmente como ajudar os necessitados.Pedidos de ajuda se sentir como uma imposição em vez de uma oportunidade para a auto-realização.
+        Pessoas altruístas acham que ajudar outras pessoas é genuinamente gratificante. Conseqüentemente, eles geralmente estão dispostos a ajudar aqueles que precisam. Pessoas altruístas descobrem que fazer coisas para os outros é uma forma de autorrealização, em vez de auto-sacrifício. Pessoas com pontuação baixa nessa escala não gostam de ajudar os necessitados. Os pedidos de ajuda parecem mais uma imposição do que uma oportunidade para a autorrealização.
       </p>
 
       <div class="title">
         Cooperação
       </div>
       <p>
-        Indivíduos que pontuam altos nesta escala não gostam de confrontos.Eles estão perfeitamente dispostos a comprometer ou negar suas próprias necessidades para se dar bem com os outros.Aqueles que marcam baixo nessa escala são mais propensos a intimidar os outros para obter o seu caminho.
+        Indivíduos com pontuação alta nesta escala não gostam de confrontos. Eles estão perfeitamente dispostos a transigir ou negar suas próprias necessidades para se darem bem com os outros. Aqueles que têm pontuação baixa nessa escala são mais propensos a intimidar os outros para conseguir o que querem.
       </p>
 
       <div class="title">
         Modéstia
       </div>
       <p>
-        Os maiores marcadores nesta escala não gostam de reivindicar que são melhores do que outras pessoas.Em alguns casos, essa atitude pode derivar de baixa autoconfiança ou auto-estima.No entanto, algumas pessoas com auto-estima alta acham imodestada inesperada.Aqueles que estão dispostos a se descrever como superiores tendem a ser vistos como desagradavelmente arrogantes por outras pessoas.
+        Pessoas com pontuação alta nesta escala não gostam de afirmar que são melhores do que as outras pessoas. Em alguns casos, essa atitude pode ser derivada de baixa autoconfiança ou autoestima. No entanto, algumas pessoas com elevada auto-estima consideram a falta de recato imprópria. Aqueles que estão dispostos a se descrever como superiores tendem a ser vistos como desagradavelmente arrogantes pelas outras pessoas.
       </p>
 
       <div class="title">
         Simpatia
       </div>
       <p>
-        As pessoas que pontuam altas nesta escala são sensíveis e compassivas.Eles sentem a dor dos outros vicariamente e são facilmente movidos para pena.Artilheiros baixos não são afetados fortemente pelo sofrimento humano.Eles se orgulham de fazer julgamentos objetivos com base na razão.Eles estão mais preocupados com a verdade e a justiça imparcial do que com misericórdia.
+        Pessoas com pontuação alta nesta escala são compassivas e compassivas. Eles sentem a dor dos outros indiretamente e são facilmente levados à piedade. Pessoas com pontuação baixa não são afetadas fortemente pelo sofrimento humano. Eles se orgulham de fazer julgamentos objetivos com base na razão. Eles estão mais preocupados com a verdade e a justiça imparcial do que com a misericórdia.
       </p>
     </v-card-text>
   </v-card>
